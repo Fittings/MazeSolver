92af0042a0b3d3463e26270fb3954d475ef556f1
Cameron Milsom <cameron.live@gmail.com>
2015-09-29 15:05:24 +0100

    Added documentation and more detailed comments.
    Added javadoc formatting. Tidied up commenting.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,10 +1,24 @@
-
 package mazesolver;
 
 
-
+/**
+* This class takes a maze and starting/end position and tries to find a solution.
+*
+* @author Cameron Milsom
+*/
 public class MazeSolver {
 	
+	/**
+	* Takes an array of the specified maze format and attempts to find the end from the start.
+	* 0 - Free space, 1 - Blocked space, 2 - Starting position, 3 - Traversed position, 4 - Ending position.
+	* 
+	* @param maze_array - an array designed to be a maze
+	* @param staX Starting X position
+	* @param staY Starting Y position
+	* @param endX Goal X position
+	* @param endY Goal Y position
+	* @return Returns a modified array that represents a solution to the exit
+	*/
 	public static int[][] solveMaze(int[][] maze_array,int staX, int staY, int endX, int endY) {
 		int[][] maze_sol = maze_array.clone();
 		maze_sol[staY][staX] = 2; //2 meaning Start.
@@ -14,24 +28,26 @@ public class MazeSolver {
 		return maze_array; //No solution
 	}
 	
-	//Lets do this using backtracking~!
-	public static boolean trySolveMaze(int[][] maze_array,int currX, int currY, int endX, int endY) {
+	/**
+	* Attempts to solve a maze from the starting values to the ending values.
+	* This uses a backtracking algorithm to fall back if it reaches a fail state
+	*/
+	private static boolean trySolveMaze(int[][] maze_array,int currX, int currY, int endX, int endY) {
 		if (currX == endX && currY == endY) {
-			maze_array[currY][currX] = 4; //4 meanign End.
+			maze_array[currY][currX] = 4; //4 Meaning End.
 			return true; //We have reached the end
 		}
-		int tempX = currX, tempY = currY;
-		//Try North, try East, Try South, Try West
 		
-		//North
-		tempY = currY-1; tempX = currX;
+		
+		//Try North
+		int tempY = currY-1; int tempX = currX;
 		if (tempY >= 0 && maze_array[tempY][tempX] == 0) {
 			maze_array[tempY][tempX] = 3; //3 Meaning X
 			if (trySolveMaze(maze_array, tempX, tempY, endX, endY)) return true; //Solved
 			maze_array[tempY][tempX] = 0; //Failed, return to previous state
 		}
 		
-		//East
+		//Try East
 		tempY = currY; tempX = currX+1;
 		if (tempX < maze_array[tempY].length && maze_array[tempY][tempX] == 0) {
 			maze_array[tempY][tempX] = 3; //3 Meaning X
@@ -39,7 +55,7 @@ public class MazeSolver {
 			maze_array[tempY][tempX] = 0; //Failed, return to previous state
 		}
 		
-		//South
+		//Try South
 		tempY = currY+1; tempX = currX;
 		if (tempY < maze_array.length && maze_array[tempY][tempX] == 0) {
 			maze_array[tempY][tempX] = 3;
@@ -47,7 +63,7 @@ public class MazeSolver {
 			maze_array[tempY][tempX] = 0; //Failed, return to previous state
 		}
 		
-		//West
+		//Try West
 		tempY = currY; tempX = currX-1;
 		if (tempX >= 0 && maze_array[tempY][tempX] == 0) {
 			maze_array[tempY][tempX] = 3;
@@ -55,13 +71,13 @@ public class MazeSolver {
 			maze_array[tempY][tempX] = 0; //Failed, return to previous state
 		}
 		
-		return false; //No movements for this iteration
+		return false; //No valid movements, Failed at this location
 	}
 	
 	
 	
 
-}
+} //end class
 
 
 
--- src/MazeSolverApp.java
+++ src/MazeSolverApp.java
@@ -3,23 +3,30 @@ import java.lang.StringBuilder; //StringBuilder
 import java.util.Scanner; //Scanner
 import mazesolver.MazeSolver; //solveMaze
 
-
-
+/**
+* This is the application class that runs the MazeSolver.
+* Runs this and give it a specified file to find a solution to a maze.
+*
+* @author Cameron Milsom
+*/
 public class MazeSolverApp {
 	static int x_max, y_max, x_sta, y_sta, x_end, y_end;
 	static int[][] maze_array;
 
-
-
+	/**
+	* Takes puzzle input from user input at command line and tries to solve it.
+	* @param args Not used
+	*/
 	public static void main(String[] args) {
 		handleInput();
 		int[][] maze_sol = MazeSolver.solveMaze(maze_array, x_sta, y_sta, x_end, y_end);
 		System.out.println(mazeToString(maze_sol));
-		
-
 	}
 	
-	//this is assuming input is perfect as specified.
+	/**
+	* Takes input from user input at command line. 
+	* This assumes the user input is perfectly clean to the specifications as there is no validation.
+	*/
 	private static void handleInput() {
 		Scanner input = new Scanner(System.in);
 
@@ -38,7 +45,10 @@ public class MazeSolverApp {
 		input.close();	
 	}
 	
-	
+	/** 
+	* Converts the solution to a String output that is more understandable.
+	* 'S' - Starting position, 'X' - Traversed path, '#' - Maze Wall, ' ' - Free Space 
+	*/
 	private static String mazeToString(int[][] maze) {
 		StringBuilder myString = new StringBuilder();
 		for (int x=0; x < maze[0].length; x++) {
@@ -54,11 +64,11 @@ public class MazeSolverApp {
 				} else {
 					myString.append(" ");
 				}
-			} //end for y
+			}
 			myString.append("\n");
 		}
 		return myString.toString();
 	}
-	
 
-}
+
+} //end class
